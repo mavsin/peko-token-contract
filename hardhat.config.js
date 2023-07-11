d79c50dcaa90e70c162f167dd34d7cce96d9276d
mr-new-0509 <cliffahleongtech@gmail.com>
2023-07-11 11:07:17 +0900

    verify

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,14 +6,26 @@ const { API_KEY, RPC_URL, WALLET_PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
-  defaultNetwork: 'lineaGoerli',
+  defaultNetwork: 'linea',
   networks: {
-    lineaGoerli: {
+    linea: {
       url: RPC_URL,
       accounts: [WALLET_PRIVATE_KEY]
     }
   },
   etherscan: {
-    apiKey: API_KEY,
+    apiKey: {
+      linea: API_KEY
+    },
+    customChains: [
+      {
+        network: "linea",
+        chainId: 59140,
+        urls: {
+          apiURL: "https://api-goerli.lineascan.build/api",
+          browserURL: ""
+        }
+      }
+    ]
   }
 };
